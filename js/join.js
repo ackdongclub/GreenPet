9f340e729ea4548f95ed518d5c6f490eaa30c134
sun <annjin12@naver.com>
2023-02-25 16:52:56 +0900

    + news Agree checkbox

--- js/join.js
+++ js/join.js
@@ -10,6 +10,19 @@ var datepicker = new tui.DatePicker('#wrapper', {
 var pass1 = document.getElementById('pw1');
 var pass2 = document.getElementById('pw2');
 
+// 비밀번호 유효성 체크
 function passchk() {
-    console.log('pass1 : ', pass1, ' pass2 : ', pass2);
+    console.log('pass1 : ', pass1.value, ' pass2 : ', pass2.value);
+    if(pass1.value !== pass2.value) {
+        alert('비밀번호가 동일하지 않습니다.');
+        return pass2.focus();
+    }
+}
+
+
+function newsAgree(e) {
+    document.querySelectorAll('input[type=checkbox]')
+    .forEach(e => e.checked = false);
+    // 체크 박스를 하나 체크 하면 모든 체크를 푼다.
+    e.checked = true; // 현재 선택한 박스만 선택한다
 }
