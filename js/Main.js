62b8618fdf5de777643fc6a85761137b8708ef9f
sun <annjin12@naver.com>
2023-03-13 22:24:04 +0900

    show ranker click event finish

--- js/Main.js
+++ js/Main.js
@@ -18,6 +18,13 @@ function click_rank1() {
         ranking.innerHTML = '1st';
         score.innerHTML = '10,800,000';
         nick.innerHTML = 'I HAVE A PLAN';
+    } else if(rank1.src == 'http://127.0.0.1:5500/img/3st.jpg') {
+        rank1.src = './img/1st.jpg';
+        rank2.src = './img/2st.jpg';
+        rank3.src = './img/3st.jpg';
+        ranking.innerHTML = '1st';
+        score.innerHTML = '10,800,000';
+        nick.innerHTML = 'I HAVE A PLAN';
     }
 };
 
@@ -29,7 +36,14 @@ function click_rank2() {
         ranking.innerHTML = '3st';
         score.innerHTML = '1,880,000';
         nick.innerHTML = 'GOOD PLAN';
-    } else if(rank1.src == 'http://127.0.0.1:5500/img/2st.jpg'){
+    } else if(rank1.src == 'http://127.0.0.1:5500/img/3st.jpg') {
+        rank1.src = './img/2st.jpg';
+        rank2.src = './img/1st.jpg';
+        rank3.src = './img/3st.jpg';
+        ranking.innerHTML = '2st';
+        score.innerHTML = '9,810,000';
+        nick.innerHTML = 'HOW EVER GLOW';
+    } else if(rank1.src == 'http://127.0.0.1:5500/img/2st.jpg') {
         rank1.src = './img/3st.jpg';
         rank2.src = './img/1st.jpg';
         rank3.src = './img/2st.jpg';
